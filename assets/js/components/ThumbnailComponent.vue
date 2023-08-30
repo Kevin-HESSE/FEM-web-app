4156dae0e765ffafcf5590c5d0ede6f696f076c9
Kévin HESSE <108680561+Kevin-HESSE@users.noreply.github.com>
2023-08-30 16:45:25 +0200

    Feature/vue integration (#1)
    * Installing vue and eslint
    
    * creation of thumbnail components

---
+++ assets/js/components/ThumbnailComponent.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="thumbnail">
+    <PictureComponent :is-trending="videoItem.isTrending" :videoItem="videoItem" />
+
+    <BookmarkFlagComponent :film-id="videoItem.id" :is-bookmarked="videoItem.isBookmarked"/>
+
+    <div class="thumbnail__description">
+      <p class="thumbnail__description-information">
+        2019
+        <span class="category">
+          <img src="/build/images/icon-nav-movies.svg" alt="Icon of movies">
+          {{ videoItem.category.name}}
+        </span>
+        {{ videoItem.rating.name }}
+      </p>
+      <p class="thumbnail__description-title">{{ videoItem.title }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import PictureComponent from '@/components/PictureComponent.vue';
+import BookmarkFlagComponent from '@/components/BookmarkFlagComponent.vue';
+
+defineProps({
+  videoItem: {
+    type: Object,
+  },
+});
+
+</script>
---
+++ assets/js/videos.js
@@ -0,0 +1,4 @@
+import { createApp } from 'vue';
+import App from './pages/VideosPage.vue';
+
+createApp(App).mount('#app');
