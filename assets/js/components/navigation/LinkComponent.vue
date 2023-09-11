5b936dc067ef7533d893fffc1f5c338fd29b83ba
Kévin HESSE <108680561+Kevin-HESSE@users.noreply.github.com>
2023-09-11 08:53:56 +0200

    Feature/navigation bar (#4)
    * configuring navbar with vue

---
+++ assets/js/components/navigation/LinkComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <a
+    :href="url"
+    :class="classComponent"
+  >
+    <slot></slot>
+  </a>
+</template>
+
+<script setup>
+
+import { computed } from 'vue';
+
+const props = defineProps({
+  url: {
+    type: String,
+    required: true,
+  },
+});
+
+const classComponent = computed(() => {
+  const classArray = ['nav__item'];
+
+  if (props.url === window.location.pathname) {
+    classArray.push('is-active');
+  }
+
+  return classArray;
+});
+</script>
